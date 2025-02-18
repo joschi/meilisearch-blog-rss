7c0d733b6d7f69ea9d710e373f8371047e66ffc3
Jochen Schalanda <jochen@schalanda.name>
2025-02-18 17:52:13 +0100

    Update feed.bash

--- feed.bash
+++ feed.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e -u -o pipefail
 
-curl -fsSO 'https://www.meilisearch.com/_next/data/ry4vh48P82l7vbo3gRpvR/blog.json'
+curl -fsSO 'https://www.meilisearch.com/_next/data/SA-Pq9MJOjQRoBjND5Mjj/blog.json'
 mkdir -p _site
 jq -f filter.jq blog.json > _site/feed.json
